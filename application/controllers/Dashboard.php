3afe6e28555a75c731dd1ca6a6fc3641250e24ca
isha handa <isha.handa@rackless.com>
2022-07-22 12:27:46 +0530

    first commit

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class dashboard extends CI_Controller {
+    //Validating login
+    function __construct(){
+        parent::__construct();
+        if(!$this->session->userdata('uid'))
+        redirect('login');
+    }
+    public function index(){
+        $userfname=$this->session->userdata('fname');	
+
+        /**getting Verified User Count */
+        $query=$this->db->where('is_verified', 1);
+        $query=$this->db->where('status', 1);
+        $query=$this->db->get('tblusers');
+        $result=$query->result();
+        $activeVerifiedUserCount=$query->num_rows();
+
+        /**getting Active Product Count */
+        $query=$this->db->where('status', 1);
+        $query=$this->db->get('tblproducts');
+        $result=$query->result();
+        $activeProductCount=$query->num_rows();
+
+        // /**getting active attached Product Count */
+        // $query= $this->db->select('*');
+        // $query= $this->db->from('tblusers');
+        // $query= $this->db->join('tblproducts', 'tblproducts.id = school.id');
+        // $query=$this->db->where('status', 1);
+        // $query=$this->db->get('tblproducts');
+        // $result=$query->result();
+        // $activeProductCount=$query->num_rows();
+
+
+        /**Getting active products not belongs to any user */
+        $query= $this->db->select('t1.id');
+        $query= $this->db->from('tblproducts t1');
+        $query= $this->db->join('tbluser_products t2', 't2.product_id = t1.id', 'left');
+        $query= $this->db->where('t1.status', 1);
+        $query= $this->db->where('t2.product_id =', null);
+        $result=$query->get();
+        $activeUnattachedProducts=$result->num_rows();
+
+        $this->load->view('dashboard',['firstname'=>$userfname,'activeVerifiedUserCount'=> $activeVerifiedUserCount,'activeProductCount' =>$activeProductCount,'activeUnattachedProducts'=>$activeUnattachedProducts]);
+    }
+
+}
---
+++ application/models/Login_model.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Login_Model extends CI_Model{
+public function index($email,$password){
+$data=array(
+'Email'=>$email,
+'Password'=>$password);
+$query=$this->db->where($data);
+$login=$this->db->get('tblusers');
+ if($login!=NULL){
+return $login->row();
+ }  
+}}
